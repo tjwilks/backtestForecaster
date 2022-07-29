65cfa1fc2b1979b1845bc1d873f8f8153b0eb473
tjwilks <tobywilkinson3@gmail.com>
2022-07-29 18:16:55 +0100

    Initial stage of testing module

---
+++ tests/test_models.py
@@ -0,0 +1,20 @@
+# class TestAdaptiveHedge:
+#     def test_fit(self):
+#         assert False
+#
+#     def test_predict(self):
+#         assert False
+#
+#     def test_get_weights(self):
+#         assert False
+#
+#
+# class TestFollowTheLeader:
+#     def test_fit(self):
+#         assert False
+#
+#     def test_predict(self):
+#         assert False
+#
+#     def test_get_weights(self):
+#         assert False
---
+++ tests/test_utils.py
@@ -0,0 +1,20 @@
+# class TestModelLoader:
+#     def test_load_models(self):
+#         assert False
+#
+#     def get_model_name(self):
+#         assert False
+#
+#     def get_model(self):
+#         assert False
+#
+#     def check_arguments(self):
+#         assert False
+#
+#
+# class TestModelInfoExtractor:
+#     def get_primitive_model_weights_all_combiners(self):
+#         assert False
+#
+#     def get_hyp_param_df(self):
+#         assert False
